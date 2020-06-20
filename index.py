52ef10454685abe55023e8400da33dece9e52ef8
Mahalinoro <52964908+Mahalinoro@users.noreply.github.com>
2020-06-20 09:23:35 +0300

    Update index.py

--- index.py
+++ index.py
@@ -80,6 +80,18 @@ def send_message():
     )
                         
     return jsonify(response_text)
+  
+ def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'movie-bot-dbfcdq-f3d6595b2774.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
 
 # run Flask app
 if __name__ == "__main__":
